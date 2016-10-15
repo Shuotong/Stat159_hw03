57be5c2c141ea59c7549b05ba393dc9a55c3c813
GamaWu <spritewu@berkeley.edu>
2016-10-14 17:18:50 -0700

    finsish regression script and wrap up makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,36 @@
-.PHONY: data
+.PHONY: data tests eda regression report clean all
 
+eda_script = code/scripts/eda-script.R
+regression_script = code/scripts/regression-script.R
+data = data/Advertising.csv
+report = report/report.Rmd
+
+# download the file Advertising.csv
 data:
-	cd data && curl -O http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv
+	cd data ; curl -O http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv
+# run the unit tests of your functions
+tests: code/test-that.R code/tests/test-regression.R
+	Rscript code/test-that.R
+
+# perform the exploratory data analysis
+eda: $(eda_script) $(data)
+	Rscript eda-script.R
+
+# perform the series of regression analyses
+regression: $(regression_script) $(data)
+	Rscript $(regression_script)
+
+# display session info
+sessionInfo: code/scripts/session-info-script.R
+	Rscript $<
+# regression functions
+function: code/functions/regression-functions.R
+	Rscript $<
+
+#generate report
+report:  $(report) regression eda function
+	cd report ; Rscript -e "library(rmarkdown); render('report.Rmd','html_document')"
+
+# will delete the generated report
+clean:
+	cd report ; rm -f report.html
---
+++ code/scripts/regression-script.R
@@ -0,0 +1,41 @@
+advertising_data <- read.csv("../../data/Advertising.csv")
+
+Sales_over_TV <- lm(Sales ~ TV, data = advertising_data)
+Sales_over_Radio <- lm(Sales ~ Radio, data = advertising_data)
+Sales_over_Newspaper <- lm(Sales ~ Newspaper, data = advertising_data)
+
+multi_reg <- lm(Sales ~ TV + Radio + Newspaper, data = advertising_data)
+multi_reg_sum<-summary(multi_reg)
+
+save(Sales_over_TV,Sales_over_Radio,Sales_over_Newspaper,
+     multi_reg, multi_reg_sum, file = "../../data/regression.RData")
+
+png(file = "../../images/scatterplot-tv-sales.png")
+plot(advertising_data$TV, advertising_data$Sales, pch = 19, col = 'blue',
+     main = "Scatterplot of Sales over TV advertising budgets ")
+abline(Sales_over_TV)
+dev.off()
+
+png(file = "../../images/scatterplot-radio-sales.png")
+plot(advertising_data$Radio, advertising_data$Sales, pch = 19, col = 'blue',
+     main = "Scatterplot of Sales over Radio advertising budgets")
+abline(Sales_over_Radio)
+dev.off()
+
+png(file = "../../images/scatterplot-newspaper-sales.png")
+plot(advertising_data$Radio, advertising_data$Sales, pch = 19, col ='blue',
+     main = "Scatterplot of Sales over Newspaper advertising budgets")
+abline(Sales_over_Newspaper)
+dev.off()
+
+png(file = "../../images/residual-plot.png")
+plot(multi_reg, which = 1)
+dev.off()
+
+png(file = "../../images/normal-qq-plot.png")
+plot(multi_reg, which = 2)
+dev.off()
+
+png(file = "../../images/scale-location-plot.png")
+plot(multi_reg, which = 3)
+dev.off()
